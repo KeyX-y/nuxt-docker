e24415487020f178ee9a5f11f217e443f2f939a7
ya_xiao <ya_xiao@163.com>
2019-07-25 00:24:13 +0800

    注册模块

---
+++ components/public/header/geo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="m-geo">
+    <i class="el-icon-location"/>上海
+    <nuxt-link
+      class="changeCity"
+      to="/changeCity">
+      切换城市
+    </nuxt-link>
+    <span>[香河 廊坊 天津]</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'geo'
+}
+</script>
+
+<style lang="scss">
+</style>
